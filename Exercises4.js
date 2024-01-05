2d170494e5b14e1da1bee0bcd2bbf8482d927086
quizford <125616425+Astronaut828@users.noreply.github.com>
2024-01-05 11:19:44 -0700

    Update Exercises4.js

--- Exercises4.js
+++ Exercises4.js
@@ -19,12 +19,7 @@ function loop(value, test, update, body) {
     value = update(value);
   }
 }
-loop(
-  3,
-  (n) => n > 0,
-  (n) => n - 1,
-  console.log
-);
+loop(3, (n) => n > 0, (n) => n - 1, console.log);
 // → 3
 // → 2
 // → 1
