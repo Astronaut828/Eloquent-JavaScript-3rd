8451ee495d28e66d3895588ac0dae62fcbd444ae
quizford <125616425+Astronaut828@users.noreply.github.com>
2024-01-05 11:15:24 -0700

    Update Exercises4.js

--- Exercises4.js
+++ Exercises4.js
@@ -10,4 +10,45 @@ console.log(flatArray); // → [1, 2, 3, 4, 5, 6]
 // Using method .flat() to achieve same result.
 console.log(arrays.flat()); // → [1, 2, 3, 4, 5, 6]
 
-// Next Exercise 
+// Your own loop
+// Write a higher-order function loop that provides something like a for loop statement.
+// It takes a value, a test function, an update function, and a body function.
+function loop(value, test, update, body) {
+  while (test(value)) {
+    body(value);
+    value = update(value);
+  }
+}
+loop(
+  3,
+  (n) => n > 0,
+  (n) => n - 1,
+  console.log
+);
+// → 3
+// → 2
+// → 1
+
+// Everything
+// Implement every as a function that takes an array and a predicate function as parameters.
+// Write two versions, one using a loop ...
+function every(array, test) {
+  for (let i = 0; i < array.length; i++) {
+    if (!test(array[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(every([1, 3, 5], (n) => n < 10)); // → true
+console.log(every([2, 4, 16], n => n < 10)); // → false
+console.log(every([], n => n < 10)); // → true
+
+// and one using the some method.
+function everyMeth(array, test) {
+  return array.every((element) => test(element));
+}
+console.log(everyMeth([1, 3, 5], (n) => n < 10)); // → true
+console.log(everyMeth([2, 4, 16], n => n < 10)); // → false
+console.log(everyMeth([], n => n < 10)); // → true
+
