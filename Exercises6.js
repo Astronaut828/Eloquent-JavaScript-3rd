846577ef3a8b88b7c17e68f50de2c9512614b9f9
quizford <125616425+Astronaut828@users.noreply.github.com>
2024-01-19 11:07:54 -0700

    Update Exercises6.js

--- Exercises6.js
+++ Exercises6.js
@@ -26,3 +26,53 @@ do {
     }
   }
 } while (lucky == false);
+
+// The locked box
+// Consider the following (rather contrived) object:
+const box = {
+  locked: true,
+  unlock() {
+    this.locked = false;
+  },
+  lock() {
+    this.locked = true;
+  },
+  _content: [1, 2, 3],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  },
+};
+
+// It is a box with a lock. There is an array in the box, but you can get at it only when the box is unlocked.
+// Directly accessing the private _content property is forbidden.
+// Write a function called withBoxUnlocked that takes a function value as argument, unlocks the box, 
+// runs the function, and then ensures that the box is locked again before returning, regardless of whether the argument function returned normally or threw an exception.
+
+let readContent = (boxContent) => {
+  let secretBoxContent = [];
+  for (content of boxContent) {
+    secretBoxContent.push(content);
+  }
+  return secretBoxContent;
+};
+
+function withBoxUnlocked(readContent) {
+  let initialBoxState = box.locked;
+  let boxContent;
+  try {
+    if (initialBoxState) {
+      box.unlock();
+    }
+    boxContent = readContent(box.content);
+  } catch (e) {
+    throw e;
+  } finally {
+    if (initialBoxState) {
+      box.lock();
+    }
+  }
+  return boxContent;
+}
+
+console.log(withBoxUnlocked(readContent));
