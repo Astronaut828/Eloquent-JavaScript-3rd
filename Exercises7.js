ea0e5d4c28afc4a290ba8797f16e9a6338685f80
quizford <125616425+Astronaut828@users.noreply.github.com>
2024-01-24 10:11:54 -0700

    Exercises7.js

--- Exercises7.js
+++ Exercises7.js
@@ -2,7 +2,6 @@
 // Say you have a function primitiveMultiply that in 20 percent of cases multiplies two numbers
 // and in the other 80 percent of cases raises an exception of type MultiplicatorUnitFailure.
 // Write a function that wraps this clunky function and just keeps trying until a call succeeds, after which it returns the result.
-
 function primitiveMultiply(x) {
   let luckyPick = Math.round(Math.random() * 100);
   if (luckyPick <= 60 && luckyPick >= 40) {
@@ -48,7 +47,6 @@ const box = {
 // Directly accessing the private _content property is forbidden.
 // Write a function called withBoxUnlocked that takes a function value as argument, unlocks the box, 
 // runs the function, and then ensures that the box is locked again before returning, regardless of whether the argument function returned normally or threw an exception.
-
 let readContent = (boxContent) => {
   let secretBoxContent = [];
   for (content of boxContent) {
